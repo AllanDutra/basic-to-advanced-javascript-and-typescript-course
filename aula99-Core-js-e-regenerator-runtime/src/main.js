dfcc63480ba127329642616f6d9be9b554ab5aaf
AllanDutra <allandutrafeitosa14@gmail.com>
2022-01-23 16:49:10 -0300

    Importando modulos do core js e regenerator runtime para tornar possivel a execucao do codigo em navegadores antigos

---
+++ aula99-Core-js-e-regenerator-runtime/src/main.js
@@ -0,0 +1,10 @@
+// corrigindo problemas de recursos mais novos em navegadores antigos
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+// ==============================================================
+
+import executa from './modules/promises';
+
+import './assets/css/style.css';
+
+executa();
---
+++ aula99-Core-js-e-regenerator-runtime/src/modules/promises.js
@@ -0,0 +1,23 @@
+function promise() {
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+
+            console.log('Hey, sou uma promessa.');
+
+            resolve();
+
+        }, 2000);
+
+    })
+
+}
+
+export default async function () {
+
+    await promise();
+
+    console.log('terminou');
+
+}
