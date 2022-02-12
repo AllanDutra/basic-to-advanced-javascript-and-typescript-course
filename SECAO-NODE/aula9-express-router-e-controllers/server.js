9e62a74b51c27ee806f7568bd5703a958430befa
AllanDutra <allandutrafeitosa14@gmail.com>
2022-02-12 12:59:24 -0300

    feat: usando router do express e criando controllers

---
+++ SECAO-NODE/aula9-express-router-e-controllers/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.listen(3000, () => {
+
+    console.log('Acessar http://localhost:3000');
+
+    console.log('Servidor executando na porta 3000...');
+
+});
+
+// FULL MVC -> front end e back end juntos
