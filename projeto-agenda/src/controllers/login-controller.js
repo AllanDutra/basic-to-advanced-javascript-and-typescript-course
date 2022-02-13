986f97ed35d057889d406f9db2f39dc34883c192
AllanDutra <allandutrafeitosa14@gmail.com>
2022-02-13 17:58:10 -0300

    feat: criar funcao register do login controller

--- projeto-agenda/src/controllers/login-controller.js
+++ projeto-agenda/src/controllers/login-controller.js
@@ -1,5 +1,47 @@
+const Login = require('../models/login-model');
+
 exports.index = (req, res) => {
 
     res.render('login');
+
+};
+
+exports.register = async (req, res) => {
+
+    try {
+
+        const login = new Login(req.body);
+
+        await login.register();
+
+        if (login.errors.length > 0) {
+
+            req.flash('errors', login.errors);
+
+            req.session.save(() => {
+
+                return res.redirect('index');
+
+            });
+
+            return;
+
+        }
+
+        req.flash('success', 'Seu usuário foi criado com sucesso.');
+
+        req.session.save(() => {
+
+            return res.redirect('index');
+
+        })
+
+    } catch (e) {
+
+        console.log(e);
+
+        res.render('404');
+
+    }
 
 }
